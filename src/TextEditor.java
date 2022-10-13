9755ec373c2e529439c07b4ed538a5689c9099d3
zx3282 <zx32829601@gmail.com>
2022-10-14 00:09:03 +0800

    我把UI先建立好了 在做之前記得先看一下ReadMe

---
+++ src/TextEditor.java
@@ -0,0 +1,8 @@
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+public class TextEditor  extends JTextPane{
+    public static void main(String[] args) {
+        new UI().setVisible(true);
+    }
+}
