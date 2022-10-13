f1dd159ff6480481b36ac08c518ec88cd9ec9679
zx3282 <zx32829601@gmail.com>
2022-10-14 00:39:16 +0800

    新增了menubar

--- src/UI.java
+++ src/UI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 public class UI extends JFrame{
+    private final JMenuBar menuBar=new JMenuBar();
+    private  JMenu menuFile,menuEdit,menuFind,menuAbout;
     public UI(){
         super("新增文字檔案");
         setUpUIComponent();
@@ -13,6 +15,16 @@ public class UI extends JFrame{
 
     private void setUpUIComponent() {
         setSize(640, 480);
+
+        menuFile=new JMenu("檔案");
+        menuEdit=new JMenu("編輯");
+        menuFind=new JMenu("尋找");
+        menuAbout=new JMenu("關於");
+        menuBar.add(menuFile);
+        menuBar.add(menuEdit);
+        menuBar.add(menuFind);
+        menuBar.add(menuAbout);
+        setJMenuBar(menuBar);
     }
 
 
