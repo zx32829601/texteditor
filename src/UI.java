56686d47720e248cb53b9ff53b3b1aad4e0fd820
zx3282 <zx32829601@gmail.com>
2022-10-14 00:06:57 +0800

    我把UI先建立好了 在做之前記得先看一下ReadMe

--- src/UI.java
+++ src/UI.java
@@ -1,3 +1,18 @@
-public class UI {
+import javax.swing.*;
+public class UI extends JFrame{
+    public UI(){
+        super("新增文字檔案");
+        setUpUIComponent();
+        setUpEventListener();
+        setVisible(true);
+    }
+
+    private void setUpEventListener() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void setUpUIComponent() {
+        setSize(640, 480);
+    }
 
 }
