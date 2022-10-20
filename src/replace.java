46f5851902be15a9880274f3d8c35f4a8949b998
YOUXIZHOU <a641556378@gmail.com>
2022-10-20 22:43:47 +0800

    取代做完ㄌ

---
+++ src/replace.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class replace extends JFrame {
+    StringBuffer output = new StringBuffer("");
+    public replace(String input_text){
+        super("取代");
+        String ip[];
+        ip = input_text.split("\n");
+        setSize(250,300);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setVisible(true);
+        Container pn = getContentPane();
+        //設定成FlowLayout
+        FlowLayout fy = new FlowLayout();
+        pn.setLayout(null);
+
+        JLabel l = new JLabel("將");
+        pn.add(l,BorderLayout.CENTER);
+        l.setBounds(100,10,100,40);
+        //文字輸入欄位//
+        JTextField jTextField_f = new JTextField();
+        jTextField_f.setColumns(10);
+        pn.add(jTextField_f);
+        jTextField_f.setBounds(60,50,100,40);
+
+        JLabel l2 = new JLabel("取代成");
+        pn.add(l2);
+        l2.setBounds(90,90,100,40);
+        JTextField jTextField_t = new JTextField();
+        jTextField_t.setColumns(10);
+        pn.add(jTextField_t);
+        jTextField_t.setBounds(60,120,100,40);
+
+        //確定跟取消按鈕//
+        JButton confirm = new JButton("確定");
+
+
+        confirm.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+            int n = 0;
+                while(n < ip.length){
+                    output.append(ip[n].replace(jTextField_f.getText(),jTextField_t.getText()));
+                    output.append("\n");
+                    n++;
+                }
+            replace.super.dispose();
+            }
+        });
+        pn.add(confirm);
+
+        JButton cancel = new JButton("取消");
+        pn.add(cancel);
+        confirm.setBounds(30,180,60,20);
+        cancel.setBounds(130,180,60,20);
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                replace.super.dispose();
+            }
+        });
+        setContentPane(pn);
+    }
+    public String getop(){
+        return output.toString();
+    }
+}
