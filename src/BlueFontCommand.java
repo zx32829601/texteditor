598a1ff5ab7696163b1c558e72bc85df3755198f
Tonyao0924 <mingyaochen0924@gmail.com>
2022-10-27 00:39:41 +0800

    都是我的Command別給我動

---
+++ src/BlueFontCommand.java
@@ -0,0 +1,9 @@
+public class BlueFontCommand extends Command{
+    public BlueFontCommand(ReceiverCommand receiverCommand) {
+        super(receiverCommand);
+    }
+    public void execute() {
+        receiverCommand.blueFont();
+    }
+
+}
