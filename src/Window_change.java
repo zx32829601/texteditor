d3bdeaaa9847a2413de33df9a88fa2fc6e75f5c5
Yang <s0931861498@gmail.com>
2022-10-27 20:34:51 +0800

    abstract factory 改良版

--- src/Window_change.java
+++ src/Window_change.java
@@ -26,23 +26,38 @@ public class Window_change implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         ModelFactory m;
+        ModelFactory2 text;
+        ModelFactory2 bg;
         String s = e.getActionCommand();
         if(s.equals("一般模式")){
             m = new NormalModel(this.co);
             m.setWindowsBackground();
             m.setTextColor();
+            /*
+            text = new Text(this.co);
+            bg = new BG(this.co);
+            text.Normal();
+            bg.Normal();
+
+             */
         }else if(s.equals("深色模式")){
             m = new DarkModel(this.co);
             m.setWindowsBackground();
             m.setTextColor();
-        }
+            /*
+            text = new Text(this.co);
+            bg = new BG(this.co);
+            text.Dark();
+            bg.Dark();
 
+             */
+        }
     }
 }
 
 abstract class ModelFactory{
-    public abstract void setWindowsBackground();
-    public abstract void setTextColor();
+    abstract void setWindowsBackground();
+    abstract void setTextColor();
 }
 
 class NormalModel extends ModelFactory {
@@ -77,7 +92,6 @@ class DarkModel extends ModelFactory {
     public DarkModel(UI ui) {
         this.ui = ui;
     }
-
     @Override
     public void setWindowsBackground() {
         Color c = Color.darkGray;
@@ -97,5 +111,61 @@ class DarkModel extends ModelFactory {
         ui.menuFont.setForeground(c);
         ui.menuHighLighter.setForeground(c);
         ui.menuModel.setForeground(c);
+    }
+}
+
+abstract class ModelFactory2{
+    abstract void Normal();
+    abstract void Dark();
+}
+
+class Text extends ModelFactory2 {
+    UI ui;
+    public Text(UI ui){
+        this.ui = ui;
+    }
+    @Override
+    public void Normal(){
+        Color c = Color.black;
+        ui.textPane.setForeground(c);
+        ui.menuFile.setForeground(c);
+        ui.menuEdit.setForeground(c);
+        ui.menuFind.setForeground(c);
+        ui.menuAbout.setForeground(c);
+        ui.menuColor.setForeground(c);
+        ui.menuFont.setForeground(c);
+        ui.menuHighLighter.setForeground(c);
+        ui.menuModel.setForeground(c);
+    }
+    @Override
+    public void Dark(){
+        Color c = Color.white;
+        ui.textPane.setForeground(c);
+        ui.menuFile.setForeground(c);
+        ui.menuEdit.setForeground(c);
+        ui.menuFind.setForeground(c);
+        ui.menuAbout.setForeground(c);
+        ui.menuColor.setForeground(c);
+        ui.menuFont.setForeground(c);
+        ui.menuHighLighter.setForeground(c);
+        ui.menuModel.setForeground(c);
+    }
+}
+class BG extends ModelFactory2 {
+    UI ui;
+    public BG(UI ui){
+        this.ui = ui;
+    }
+    @Override
+    public void Normal(){
+        Color c = Color.white;
+        ui.textPane.setBackground(c);
+        ui.menuBar.setBackground(c);
+    }
+    @Override
+    public void Dark(){
+        Color c = Color.darkGray;
+        ui.textPane.setBackground(c);
+        ui.menuBar.setBackground(c);
     }
 }
