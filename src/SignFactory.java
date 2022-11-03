ce4689e9daec3c44e7aab78aceb1bcfa35b8eccf
Nokozuki <115621913+Nokozuki@users.noreply.github.com>
2022-11-03 23:03:08 +0800

    改成switchㄌ

--- src/SignFactory.java
+++ src/SignFactory.java
@@ -3,10 +3,20 @@ import javax.swing.*;
 public class SignFactory extends JFrame {
 
     public static Sign BuildSign(String SingType) {
-        if (SingType.equals("圓形"))
+        switch (SingType){
+            case "圓形":{
+                return new CircleSign();
+            }
+            case "菱形":{
+                return new RhombusSign();
+            }
+
+        }
+        return null;
+        /*if (SingType.equals("圓形"))
             return new CircleSign();
         else
-            return new RhombusSign();
+            return new RhombusSign();*/
     }
 }
 
