bc5c240d8c3d60161df2bc511c7e6c1abc0f3565
zx3282 <zx32829601@gmail.com>
2022-11-03 23:05:25 +0800

    Strategy 更動 菱形圖片

--- src/factory/SignFactory.java
+++ src/factory/SignFactory.java
@@ -5,10 +5,16 @@ import javax.swing.*;
 public class SignFactory extends JFrame {
 
     public static Sign BuildSign(String SingType) {
-        if (SingType.equals("圓形"))
-            return new CircleSign();
-        else
-            return new RhombusSign();
+        switch (SingType) {
+            case "圓形": {
+                return new CircleSign();
+            }
+            case "菱形": {
+                return new RhombusSign();
+            }
+
+        }
+        return null;
     }
 }
 
