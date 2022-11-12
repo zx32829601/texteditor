0660d795316c3b356ddbb6443180820fb0f515c8
PeNDy <b10923024@gemail.yuntech.edu.tw>
2022-11-13 01:29:29 +0800

    修開檔案

--- src/nopatternfuntion/FileEditor.java
+++ src/nopatternfuntion/FileEditor.java
@@ -93,7 +93,7 @@ public class FileEditor implements ActionListener {
     private void open() {
         // fileChooser 是 JFileChooser 的實例
         // 顯示檔案選取的對話方塊
-        int option = fileChooser.showDialog(null, null);
+        int option = fileChooser.showOpenDialog(null);
 
         // 使用者按下確認鍵
         if (option == JFileChooser.APPROVE_OPTION) {
@@ -151,7 +151,7 @@ public class FileEditor implements ActionListener {
 
     private void saveFileAs() {
         // 顯示檔案對話方塊
-        int option = fileChooser.showDialog(null, null);
+        int option = fileChooser.showSaveDialog(null);
         // 如果確認選取檔案
         if (option == JFileChooser.APPROVE_OPTION) {
             // 取得選擇的檔案
